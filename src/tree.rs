0699df84766d8ab7c0dfb2b951f9d6518a649535
Achal Dave <achalddave@live.com>
2015-07-03 21:28:33 -0700

    Added comment about lifetime issue

--- src/tree.rs
+++ src/tree.rs
@@ -14,6 +14,8 @@ fn print_dir_recursive(path : &str, indent_level : usize)
 
     let paths = try!(fs::read_dir(path));
     for entry in paths {
+        // For some reason, try!(entry).path().to_str().unwrap() complains about
+        // some lifetime issue? I don't understand this yet.
         let entry_name = try!(entry).path();
         let entry_name = entry_name.to_str().unwrap();
         println!("{}- {}", indent, entry_name);
